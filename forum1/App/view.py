af9f7d6de1f2c03dcb8b1927230bef72747837bf
ddd <ddd@jdjdjd.com>
2019-06-15 09:31:37 +0800

    1

--- forum1/App/view.py
+++ forum1/App/view.py
@@ -8,21 +8,54 @@ bbs=Blueprint('bbs',__name__)
 # def index():
 #     return render_template('index.html')
 @bbs.route('/')
-@bbs.route('/<int:cid>')#php技术交流/程序人生
-def index (cid=0):
-    if cid == 0:
-        bz = Category.query.filter(Category.parentid == cid).all()
-    else:
-        bz = Category.query.filter(Category.cid == cid).all()
-    # 所有大版块
-    allBk = Category.query.filter(Category.parentid == 0).all()
-    # 小版块
-    smallbz = Category.query.filter(Category.parentid != 0).all()
-    return render_template('index.html', **{
-        'category': bz,
-        'allBk': allBk,
-        'smalls': smallbz
-    })
+@bbs.route('/<int:cid>')
+@bbs.route('/<int:cid>/<int:xid>')#php技术交流/程序人生
+def index (cid=0,xid=0):
+    if xid == 0:
+        if cid == 0:
+            bz = Category.query.filter(Category.parentid == cid).all()
+        else:
+            bz = Category.query.filter(Category.cid == cid).all()
+        # 所有大版块
+        allBk = Category.query.filter(Category.parentid == 0).all()
+        # 小版块
+        smallbz = Category.query.filter(Category.parentid != 0).all()
+        return render_template('index.html', **{
+            'category': bz,
+            'allBk': allBk,
+            'smalls': smallbz,
+            'iid':cid,
+            'id':xid
+        })
+    else :
+
+        allBK = Category.query.filter(Category.parentid == 0).all()
+        smallbz = Category.query.filter(Category.parentid != 0).all()
+        BK = Category.query.filter(Category.parentid == cid).all() #allBK
+        data = Category.query.filter(Category.cid==xid).all()
+        # for n in range(3):
+        #     print(allBK[n].classname)
+        list=[]
+        for ank in allBK:
+            if ank.cid == cid:
+                list.append(ank)
+                for ak in smallbz:
+                    if ak.cid == xid:
+                        list.append(ak)
+        return render_template('index.html',**{
+            'allBk': allBK,
+            'smalls': smallbz,
+            'bk':BK,
+            'da':data,
+            'iid':cid,
+            'id':xid,
+            'li':list
+        })
+
+
+
+
+
 @bbs.route('/set')
 def set_session():
     session['username']='admin'
